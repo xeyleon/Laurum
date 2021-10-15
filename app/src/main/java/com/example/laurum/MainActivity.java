5360777122008a9e3b434d04880f13e04e8e5bfe
xeyleon <49344102+xeyleon@users.noreply.github.com>
2021-10-14 21:36:30 -0400

    Added a tab layout to main (A possible option)

--- app/src/main/java/com/example/laurum/MainActivity.java
+++ app/src/main/java/com/example/laurum/MainActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Laurum);
         setContentView(R.layout.activity_main);
+
+        //Remove Title Bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Populate tabs
+        tabLayout = findViewById(R.id.MainTabLayout);
+        TabLayout.Tab reminderTab = tabLayout.newTab();
+        reminderTab.setText("Reminders"); // set the text for the tab
+        reminderTab.setIcon(R.drawable.ic_reminders_tab); // set an icon for the tab
+        tabLayout.addTab(reminderTab);
     }
 }
