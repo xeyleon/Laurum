c3d33519a4c8962d55b82674d6f4952688651598
xeyleon <49344102+xeyleon@users.noreply.github.com>
2021-10-17 23:22:27 -0400

    Some database setup updates

---
+++ app/src/main/java/com/laurum/Resources/Resource.java
@@ -0,0 +1,39 @@
+package com.laurum.Resources;
+
+public class Resource {
+    private final Integer id;
+    private final String title;
+    private final String desc;
+    private final String url;
+
+    public Resource() {
+        this.id = -1;
+        this.title = "title";
+        this.desc = "desc";
+        this.url = "url";
+    }
+
+    public Resource(Integer id, String title, String desc, String url) {
+        this.id = -1;
+        this.title = "title";
+        this.desc = "desc";
+        this.url = "url";
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+}
